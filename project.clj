3c5c965029519104befcc03a1f4b5b8a8e0d4370
Gareth <gclapperton@live.com>
2015-05-13 15:24:31 -0400

    Updated dependencies and bumped version

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject views/honeysql "0.1.0-SNAPSHOT"
+(defproject views/honeysql "0.1.1-SNAPSHOT"
   :description "HoneySQL view implementation for views"
   :url "https://github.com/kirasystems/views-honeysql"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[views "1.4.0-SNAPSHOT"]
-                 [honeysql "0.5.2"]
+  :dependencies [[views "1.4.1-SNAPSHOT"]
+                 [honeysql "0.6.0"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [org.clojure/tools.logging "0.3.1"]]
+  :plugins [[lein-ancient "0.6.7"]]
   :profiles {:dev {:dependencies
                    [[org.clojure/clojure "1.6.0"]]}})
